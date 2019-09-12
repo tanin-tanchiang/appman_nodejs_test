eca19e8063d442bd3eff793526991c6cd84d8fc8
Tanin Tanchiang <tanin.tanchiang@gmail.com>
2019-09-12 20:39:36 +0700

    problem 1 complete

--- problems/problem_01/three-sum.js
+++ problems/problem_01/three-sum.js
@@ -1,6 +1,16 @@
 const threeSum = (nums, target) => {
   // put your code here !!
-  return [];
+  let results = [];
+  for(let i = 0; i < nums.length; i++){
+    for(let j = i+1; j < nums.length; j++){
+      for(let k = j+1; k< nums.length; k++){
+        if(nums[i] + nums[j] + nums[k] == target){
+          results.push([i, j , k]);
+        }
+      }
+    }
+  }
+  return results;
 };
 
 module.exports = { threeSum };
