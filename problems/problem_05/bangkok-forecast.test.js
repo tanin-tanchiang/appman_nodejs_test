0d5df1d4d9fd0a8121905fa43075a1fd2df0baf4
Tanin Tanchiang <tanin.tanchiang@gmail.com>
2019-09-12 21:42:12 +0700

    no message

--- problems/problem_05/bangkok-forecast.test.js
+++ problems/problem_05/bangkok-forecast.test.js
@@ -1,1 +1,16 @@
-// please write test here !!
+const { expect } = require('chai');
+const { bangkokForecast } = require('./index');
+
+describe('Problem 05 - bangkok-forecast', () => {
+  const testcases = [
+    {
+      expected: [111]
+    },
+  ];
+  testcases.forEach(tc => {
+    it(`${tc.expected}`, () => {
+      const result = bangkokForecast();
+      expect(result).to.be.eq(tc.expected);
+    });
+  });
+});
